0d28f095b6304345204dac70ffb8de6749cecac1
r.cavalcante <r.cavalcante@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-08-26 15:19:49 +0000

    Fix NOTES from setup.R
    * Reformat code (tabs equiv. to 4 spaces)
    * Add :: to calls
    * Use df_name$column formulation in subset() rather than column.
    
    From: Raymond Cavalcante <cavalcante.raymond@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipenrich@120498 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/setup.R
+++ R/setup.R
@@ -1,107 +1,103 @@
 filter_genesets = function(x,max_geneset_size = 2000) {
-  x_class = class(x);
-  x_class_attr = attr(x_class,"package");
+	x_class = class(x)
+	x_class_attr = attr(x_class, "package")
 
-  if (is.null(x_class) || is.null(x_class_attr)) {
-    stop("Error: bad geneset object in filtering step..");
-  }
+	if (is.null(x_class) || is.null(x_class_attr)) {
+		stop("Error: bad geneset object in filtering step..")
+	}
 
-  if (x_class != "GeneSet" || x_class_attr != "chipenrich.data") {
-    stop("Error: bad geneset object in filtering step..");
-  }
+	if (x_class != "GeneSet" || x_class_attr != "chipenrich.data") {
+		stop("Error: bad geneset object in filtering step..")
+	}
 
-  g = as.list(x@set.gene);
-  g = Filter(function(x) length(x) <= max_geneset_size,g);
-  x@set.gene = as.environment(g);
+	g = as.list(x@set.gene)
+	g = Filter(function(x) length(x) <= max_geneset_size, g)
+	x@set.gene = as.environment(g)
 
-  return(x);
+	return(x)
 }
 
 read_mappa = function(file_path) {
-  if (!file.exists(file_path)) {
-    stop("Error: could not find mappability file: ",file_path);
-  }
+	if (!file.exists(file_path)) {
+		stop("Error: could not find mappability file: ", file_path)
+	}
 
-	d = read.table(file_path,sep="\t",header=T,stringsAsFactors=F);
+	d = read.table(file_path, sep = "\t", header = T, stringsAsFactors = F)
 
 	# Check columns.
-	for (col in c("geneid","mappa")) {
+	for (col in c("geneid", "mappa")) {
 		if (!col %in% names(d)) {
-			stop(sprintf("Error reading mappability data: no column named '%s' in file.",col));
+			stop(sprintf("Error reading mappability data: no column named '%s' in file.",col))
 		}
 	}
 
 	# Genes in this file should not be duplicated.
 	if (sum(duplicated(d$geneid)) > 0) {
-		stop("Error reading mappability data: duplicate geneids exist in file.");
+		stop("Error reading mappability data: duplicate geneids exist in file.")
 	}
 
-  # Mappability should be between 0 and 1.
+	# Mappability should be between 0 and 1.
 	if (min(d$mappa) < 0) {
-    stop("Error: mappability must be >= 0.");
-  }
+		stop("Error: mappability must be >= 0.")
+	}
 
-  if (max(d$mappa) > 1) {
-    stop("Error: mappability must be <= 1.");
-  }
+	if (max(d$mappa) > 1) {
+		stop("Error: mappability must be <= 1.")
+	}
 
-	return(d);
+	return(d)
 }
 
 # Creates a LocusDefinition object given a flat file of definitions.
 setup_ldef = function(filepath) {
-  # Check if file exists.
-  if (!file.exists(filepath)) {
-    stop("Error: unable to open file for reading: ",filepath);
-  }
+	# Check if file exists.
+	if (!file.exists(filepath)) {
+		stop("Error: unable to open file for reading: ", filepath)
+	}
 
-  # Read in flat file.
-  message("Reading user-specified gene locus definitions: ",filepath);
-  d = read.table(filepath,sep="\t",header=T,stringsAsFactors=F);
+	# Read in flat file.
+	message("Reading user-specified gene locus definitions: ", filepath)
+	d = read.table(filepath, sep = "\t", header = T, stringsAsFactors = F)
 
-  # Warn if missing entries, we'll ignore them though.
-  num_missing = sum(!complete.cases(d));
-  if (num_missing > 0) {
-    message(sprintf("Warning: %i rows of user-provided locus definition were missing values, skipping these rows..",num_missing));
-  }
+	# Warn if missing entries, we'll ignore them though.
+	num_missing = sum(!complete.cases(d))
+	if (num_missing > 0) {
+		message(sprintf("Warning: %i rows of user-provided locus definition were missing values, skipping these rows..", num_missing))
+	}
 
-  # Remove rows with missing data.
-  d = na.omit(d);
+	# Remove rows with missing data.
+	d = na.omit(d)
 
-  filename = strip_ext(filepath);
+	filename = strip_ext(filepath)
 
-  # Create new locus definition object.
-  object = new("LocusDefinition");
+	# Create new locus definition object.
+	object = new("LocusDefinition")
 
-  # Remove duplicated rows, not duplicated genes.
-  # Some genes will exist on multiple rows because they are split by
-  # other transcripts, or small nuclear RNAs, etc.
-  d = unique(d);
+	# Remove duplicated rows, not duplicated genes.
+	# Some genes will exist on multiple rows because they are split by
+	# other transcripts, or small nuclear RNAs, etc.
+	d = unique(d)
 
-  # Create an IRanges object representing the loci for each gene on that chromosome.
-  chroms = list();
-  for (chr in unique(d$chrom)) {
-    genes_chrom = subset(d,chrom == chr);
-    chroms[[chr]] = IRanges(start=genes_chrom$start,end=genes_chrom$end,names=genes_chrom$geneid);
-  }
+	# Create an IRanges object representing the loci for each gene on that chromosome.
+	chroms = list()
+	for (chr in unique(d$chrom)) {
+		genes_chrom = subset(d, d$chrom == chr)
+		chroms[[chr]] = IRanges::IRanges(
+			start = genes_chrom$start,
+			end = genes_chrom$end,
+			names = genes_chrom$geneid)
+	}
 
-  object@dframe = d;
-  object@chrom2iranges = chroms;
+	object@dframe = d
+	object@chrom2iranges = chroms
 
-  # Store as GRanges object as well, for convenience.
-  object@granges = GRanges(
-    seqnames=d$chrom,
-    ranges=IRanges(d$start,d$end),
-    names=d$geneid
-  );
+	# Store as GRanges object as well, for convenience.
+	object@granges = GenomicRanges::GRanges(
+		seqnames = d$chrom,
+		ranges = IRanges::IRanges(start = d$start, end = d$end),
+		names = d$geneid)
 
-  # Check to make sure locus definitions are disjoint - that is, they do not overlap each other.
-  # SLATED FOR DELETION PENDING CONSEQUENCES
-#  if(!isDisjoint(object@granges)) {
-#    stop("Error: user-provided locus definitions overlap - there should be disjoint ranges for all genes.");
-#  }
-
-  object;
+	object
 }
 
 # Creates an object that mimics the GeneSet class
@@ -128,51 +124,53 @@ setup_ldef = function(filepath) {
 # name and the second column being Entrez Gene ID
 setup_geneset = function(filepath) {
 
-    # Check if file exists.
-    if (!file.exists(filepath)) {
-        stop("Error: unable to open file for reading: ",filepath)
-    }
+	# Check if file exists.
+	if (!file.exists(filepath)) {
+		stop("Error: unable to open file for reading: ", filepath)
+	}
 
-    # Read in flat file.
-    message("Reading user-specified gene set definitions: ",filepath)
-    d = read.table(filepath,sep="\t",header=T,stringsAsFactors=F)
+	# Read in flat file.
+	message("Reading user-specified gene set definitions: ", filepath)
+	d = read.table(filepath, sep = "\t", header = T, stringsAsFactors = F)
 
-    # Warn if missing entries, we'll ignore them though.
-    num_missing = sum(!complete.cases(d));
-    if (num_missing > 0) {
-        message(sprintf("Warning: %i rows of user-provided locus definition were missing values, skipping these rows..",num_missing))
-    }
+	# Warn if missing entries, we'll ignore them though.
+	num_missing = sum(!stats::complete.cases(d))
+	if (num_missing > 0) {
+		message(sprintf("Warning: %i rows of user-provided locus definition were missing values, skipping these rows..", num_missing))
+	}
 
-    # Remove rows with missing data.
-    d = na.omit(d)
+	# Remove rows with missing data.
+	d = stats::na.omit(d)
 
-    filename = strip_ext(filepath)
+	filename = strip_ext(filepath)
 
-    # Create shell for geneset list
-    gs.names = unique(d[,1])
-    gs = as.list(gs.names)
-    names(gs) = gs.names
+	# Create shell for geneset list
+	# Use numerical column accessors
+	gs.names = unique(d[,1])
+	gs = as.list(gs.names)
+	names(gs) = gs.names
 
-    gs.names.list = gs
+	gs.names.list = gs
 
-    # Populate the shell
-    gs = lapply(gs, function(g){
-        return(unique(subset(d, d[,1] == g)[,2]))
-    })
+	# Populate the shell
+	# Use numerical column accessors
+	gs = lapply(gs, function(g){
+		return(unique(subset(d, d[,1] == g)[,2]))
+	})
 
-    # Create new GeneSet object
-    object = new('GeneSet')
+	# Create new GeneSet object
+	object = new('GeneSet')
 
-    # Populate the GeneSet object
-    object@type = 'user-supplied'
-    object@organism = 'user-supplied'
-    object@dburl = 'user-supplied'
+	# Populate the GeneSet object
+	object@type = 'user-supplied'
+	object@organism = 'user-supplied'
+	object@dburl = 'user-supplied'
 
-    object@set.gene = as.environment(gs)
-    object@all.genes = as.character(sort(Reduce(function(x,y) union(x,y),object@set.gene)))
+	object@set.gene = as.environment(gs)
+	object@all.genes = as.character(sort(Reduce(function(x,y) union(x,y), object@set.gene)))
 
-    object@set.name = as.environment(gs.names.list)
+	object@set.name = as.environment(gs.names.list)
 	message('Done setting up user-specified geneset..')
 
-    return(object)
+	return(object)
 }
