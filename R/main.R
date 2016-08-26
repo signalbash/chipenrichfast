e398c568fff1727ed0c5b0786c7a7cbdfc6897e2
Raymond Cavalcante <rcavalca@umich.edu>
2016-08-26 15:17:28 +0000

    Move supported_*() functions
    From: Raymond Cavalcante <cavalcante.raymond@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipenrich@120473 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/main.R
+++ R/main.R
@@ -846,41 +846,6 @@ genome_to_organism = function(genome) {
   org;
 }
 
-supported_locusdefs = function() {
-  piqr = data(package = "chipenrich.data");
-  data_files = piqr$results[,3];
-
-  ldefs = grep("locusdef",data_files,value=T);
-  unique(str_replace(ldefs,"locusdef\\.(.+?)\\.",""));
-}
-
-supported_read_lengths = function() {
-  piqr = data(package = "chipenrich.data");
-  data_files = piqr$results[,3];
-
-  mappas = grep("mappa",data_files,value=T);
-  sort(unique(as.numeric(str_replace(str_extract(mappas,"(\\d+)mer"),"mer",""))));
-}
-
-supported_genesets = function() {
-  piqr = data(package = "chipenrich.data");
-  data_files = piqr$results[,3];
-
-  geneset_files = grep("geneset",data_files,value=T);
-  unlist(unique(Map(function(x) x[[2]],str_split(geneset_files,"\\."))));
-}
-
-supported_genomes = function() {
-  piqr = data(package = "chipenrich.data");
-  data_files = piqr$results[,3];
-
-  as.character(na.omit(unique(str_match(data_files,"(locusdef|mappa)\\.(\\w+)\\.")[,3])));
-}
-
-supported_methods = function() {
-  return(names(SUPPORTED_METHODS));
-}
-
 # Checks to see if all elements of a list argument are possible.
 check_arg = function(arg,possible_values,value=F) {
   if (!all(arg %in% possible_values)) {
