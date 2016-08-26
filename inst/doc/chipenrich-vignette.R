6b73cabba635ab8c01d6ee3e46e78f221b57bb79
Raymond Cavalcante <rcavalca@umich.edu>
2016-08-26 15:18:54 +0000

    Rewrite vignette in Rmarkdown and fixes
    * Vignette is in Rmarkdown
    * Reduced max_geneset_size in vignette and examples to speed up package
    builds
    * Moved FET and Binomial warning block in chipenrich.R.
    
    From: Raymond Cavalcante <cavalcante.raymond@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipenrich@120488 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/doc/chipenrich-vignette.R
@@ -0,0 +1,86 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  library(chipenrich)
+
+## ---- echo=FALSE---------------------------------------------------------
+suppressWarnings(suppressMessages(suppressPackageStartupMessages(devtools::load_all())))
+
+## ---- eval=FALSE---------------------------------------------------------
+#  chipenrich(peaks, out_name = "chipenrich", out_path = getwd(),
+#    genome = "hg19", genesets = c("GOBP", "GOCC", "GOMF"),
+#    locusdef = "nearest_tss", method = "chipenrich",
+#    fisher_alt = "two.sided", use_mappability = F, mappa_file = NULL,
+#    read_length = 36, qc_plots = T, max_geneset_size = 2000,
+#    num_peak_threshold = 1, n_cores = 1)
+
+## ------------------------------------------------------------------------
+data(peaks_E2F4, package = 'chipenrich.data')
+data(peaks_H3K4me3_GM12878, package = 'chipenrich.data')
+
+head(peaks_E2F4)
+
+## ------------------------------------------------------------------------
+supported_genomes()
+
+## ------------------------------------------------------------------------
+supported_locusdefs()
+
+## ------------------------------------------------------------------------
+supported_genesets()
+
+## ------------------------------------------------------------------------
+supported_methods()
+
+## ------------------------------------------------------------------------
+supported_read_lengths()
+
+## ---- fig.align='center', fig.cap='E2F4 peak distances to TSS', fig.height=6, fig.width=6, fig.show='hold'----
+plot_dist_to_tss(peaks = peaks_E2F4, genome = 'hg19')
+
+## ---- fig.align='center', fig.cap='E2F4 spline without mappability', fig.height=6, fig.width=6, fig.show='hold'----
+plot_spline_length(peaks = peaks_E2F4, locusdef = 'nearest_tss', genome = 'hg19')
+
+## ---- fig.align='center', fig.cap='E2F4 spline with mappability', fig.height=6, fig.width=6, fig.show='hold'----
+plot_spline_length(peaks = peaks_E2F4, locusdef = 'nearest_tss',  genome = 'hg19', use_mappability = T, read_length = 24)
+
+## ---- fig.align='center', fig.cap='H3K4me3 gene coverage', fig.height=6, fig.width=6, fig.show='hold'----
+plot_gene_coverage(peaks = peaks_H3K4me3_GM12878, locusdef = 'nearest_tss',  genome = 'hg19')
+
+## ------------------------------------------------------------------------
+# Without mappability
+results = chipenrich(peaks = peaks_E2F4, genesets = "kegg_pathway",
+	locusdef = "nearest_tss", max_geneset_size = 20, qc_plots = F,
+	out_name = NULL, n_cores = 1)
+results.ce = results$results
+print(results.ce[1:5,1:5])
+
+## ------------------------------------------------------------------------
+# With mappability
+results = chipenrich(peaks = peaks_E2F4,genesets = "kegg_pathway",
+	locusdef = "nearest_tss", max_geneset_size = 20, use_mappability=T,
+	read_length=24, qc_plots = F, out_name = NULL,n_cores=1)
+results.cem = results$results
+print(results.cem[1:5,1:5])
+
+## ------------------------------------------------------------------------
+results = chipenrich(peaks = peaks_H3K4me3_GM12878, genesets = "kegg_pathway",
+	method='broadenrich', locusdef = "nearest_tss", max_geneset_size = 20,
+	qc_plots = F, out_name = NULL, n_cores=1)
+results.be = results$results
+print(results.be[1:5,1:5])
+
+## ------------------------------------------------------------------------
+results = chipenrich(peaks = peaks_E2F4, genesets = c("kegg_pathway"),
+	locusdef = "5kb", method = "fet", fisher_alt = "two.sided",
+	max_geneset_size = 20, qc_plots = F, out_name = NULL)
+results.fet = results$results
+print(results.fet[1:5,1:5])
+
+## ------------------------------------------------------------------------
+head(results$peaks)
+
+## ------------------------------------------------------------------------
+head(results$peaks_per_gene)
+
+## ------------------------------------------------------------------------
+head(results$results)
+
