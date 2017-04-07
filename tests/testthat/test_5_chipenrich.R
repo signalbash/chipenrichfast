df722e177b221f6c0d4ab791601a98ada7e87741
r.cavalcante <r.cavalcante@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-07 19:08:28 +0000

    Streamline preliminary steps in chipenrich()
    * In preparation for breaking out broadenrich and polyenrich methods into their own functions, we want the internal code in chipenrich() to be as modular as possible.
    * New setup_locusdef() function is called within chipenrich() and uses parameters from it to either construct a user-defined locus definition, or load one from chipenrich.data.
    * New setup_genesets() function is called within chipenrich() and uses parameters from it to either read a user-defined gene set file or load them from chipenrich.data.
    * New setup_mappa() function does the same as the previous two functions. It checks for proper overlap with locus definitions.
    * Streamlined the mappability interface in chipenrich down to one parameter that is either NULL, a file path for custom mappability, or one of the valid read lengths in supported_read_lengths().
    * Various documentation and vignette changes to go along with this.
    
    From: Raymond Cavalcante <cavalcante.raymond@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipenrich@128327 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_5_chipenrich.R
+++ tests/testthat/test_5_chipenrich.R
@@ -9,64 +9,64 @@ peaks_H3K4me3_GM12878 = subset(peaks_H3K4me3_GM12878, peaks_H3K4me3_GM12878$chro
 gs_path = system.file('extdata','vignette_genesets.txt', package='chipenrich')
 
 test_that('Test chipenrich method without mappability', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path,
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path,
 		locusdef = "nearest_tss", qc_plots = F, out_name = NULL, n_cores = 1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test chipenrich method with mappability', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path,
-		locusdef = "nearest_tss", use_mappability=T, read_length=24, qc_plots = F,
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path,
+		locusdef = "nearest_tss", mappability = '24', qc_plots = F,
 		out_name = NULL,n_cores=1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test chipenrich_slow method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path, method = 'chipenrich_slow',
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path, method = 'chipenrich_slow',
 		locusdef = "nearest_tss", qc_plots = F, out_name = NULL, n_cores = 1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test chipapprox method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path, method = 'chipapprox',
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path, method = 'chipapprox',
 		locusdef = "nearest_tss", qc_plots = F, out_name = NULL, n_cores = 1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test polyenrich method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path, method = 'polyenrich',
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path, method = 'polyenrich',
 		locusdef = "nearest_tss", qc_plots = F, out_name = NULL, n_cores = 1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test polyenrich_slow method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path, method = 'polyenrich_slow',
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path, method = 'polyenrich_slow',
 		locusdef = "nearest_tss", qc_plots = F, out_name = NULL, n_cores = 1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test broadenrich method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_H3K4me3_GM12878, genesets = gs_path,
+	results = suppressWarnings(chipenrich(peaks = peaks_H3K4me3_GM12878, genome = 'hg19', genesets = gs_path,
 		method='broadenrich', locusdef = "nearest_tss", qc_plots = F, out_name = NULL, n_cores=1))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test FET method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path, locusdef = "5kb",
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path, locusdef = "5kb",
 		method = "fet", fisher_alt = "two.sided", qc_plots = F, out_name = NULL))
 
 	expect_equal(class(results), 'list')
 })
 
 test_that('Test binomial method', {
-	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genesets = gs_path, locusdef = "5kb",
+	results = suppressWarnings(chipenrich(peaks = peaks_E2F4, genome = 'hg19', genesets = gs_path, locusdef = "5kb",
 		method = "binomial", qc_plots = F, out_name = NULL))
 
 	expect_equal(class(results), 'list')
