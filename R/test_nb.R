a4bbed14c41267a1c8871a7e10539fb132aaeb58
r.cavalcante <r.cavalcante@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-08 23:21:42 +0000

    Remove geneset filtering based on gpw (ppg) from test*() functions
    * Code commented with: "Filter genes in the geneset to only those in the gpw table" is redundant. Moved this step to filter_genesets() so that genes smaller than a certain size after filtering for the locus definition were not tested.
    * The same code above was also commented with: "The gpw table will be truncated depending on which geneset type we're in" but I don't think this was actually true. Not that it matters.
    
    From: Raymond Cavalcante <cavalcante.raymond@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipenrich@128395 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/test_nb.R
+++ R/test_nb.R
@@ -33,10 +33,6 @@ single_gam_nb = function(go_id, geneset, gpw, method, model) {
   # Genes in the geneset
   go_genes = geneset@set.gene[[go_id]];
 
-  # Filter genes in the geneset to only those in the gpw table.
-  # The gpw table will be truncated depending on which geneset type we're in.
-  go_genes = go_genes[go_genes %in% gpw$gene_id];
-
   # Background genes and the background presence of a peak
   b_genes = gpw$gene_id %in% go_genes;
   sg_go = gpw$peak[b_genes];
