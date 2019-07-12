4a4950616350303c825eaa9953d7f1ce0e62eb45
Unknown <leetaiyi@umich.edu>
2019-07-12 10:11:42 -0400

    proxReg
    User functions
    Needs documentation

---
+++ R/proxReg.R
@@ -0,0 +1,179 @@
+#' Run Proximity Regulation test on narrow genomic regions
+#' 
+#' Description
+#' 
+#' @section Method:
+#' ..
+#' 
+#' @section ...
+#' 
+#' @param xx blah
+#' @param xx2 blah
+#' ..
+#' 
+#' @return ...
+#' 
+#' @examples 
+#' ...
+#' 
+#' @export
+#' @include constants.R utils.R supported.R setup.R randomize.R
+proxReg = function(
+	peaks,
+	out_name = "proxReg",
+	out_path = getwd(),
+	genome = supported_genomes(),
+	reglocation = "tss",
+	genesets = c(
+		'GOBP',
+		'GOCC',
+		'GOMF'),
+	qc_plots = TRUE,
+	min_geneset_size = 15,
+	max_geneset_size = 2000,
+	n_cores = 1
+) {
+	genome = match.arg(genome)
+	
+	n_cores = reset_ncores_for_windows(n_cores)
+	
+	# Kill if reglocation is not tss or enhancer
+	if (!(reglocation %in% c("tss","enhancer"))) {
+		stop("Unsupported regulatory location!")
+	}
+	
+	############################################################################
+	# Collect options for opts output
+	opts_list = as.list(sys.call())
+	opts_list = opts_list[2:length(opts_list)]
+	
+	opts = data.frame(
+		parameters = names(opts_list),
+		values = as.character(opts_list),
+		stringsAsFactors = FALSE
+	)
+	
+	############################################################################
+	# Setup locus definitions, genesets, and mappability
+	
+	#Locus definition will always be NTSS for this
+	ldef_list = setup_locusdef(ldef_code = "nearest_tss", genome, randomization = NULL)
+	ldef = ldef_list[['ldef']]
+	tss = ldef_list[['tss']]
+	
+	geneset_list = setup_genesets(gs_codes = genesets, ldef_obj = ldef, genome = genome, min_geneset_size = min_geneset_size, max_geneset_size = max_geneset_size)
+	
+	############################################################################
+	############################################################################
+	# Start enrichment process
+	############################################################################
+	############################################################################
+	
+	######################################################
+	# Read in and format peaks (from data.frame or file)
+	if (class(peaks) == "data.frame") {
+		message('Reading peaks from data.frame...')
+		peakobj = load_peaks(peaks)
+	} else if (class(peaks) == "character") {
+		peakobj = read_bed(peaks)
+	}
+	
+	# Number of peaks in data.
+	num_peaks = length(peakobj)
+	
+	######################################################
+	# Assign peaks to genes.
+	message("Assigning peaks to genes with assign_peaks(...) ..")
+	assigned_peaks = assign_peaks(peakobj, ldef, tss)
+	
+	# Creating a column for adjusted DTSS
+	if ("tss" %in% reglocation) {
+		assigned_peaks$log_dtss = log(abs(assigned_peaks$dist_to_tss)+1)
+		log_gene_ll = log(assigned_peaks$gene_locus_end-assigned_peaks$gene_locus_start)
+		pred_log_dtss = as.numeric(fitted(chipenrich.data::spline.log_dtss.90ENCODE, peaks, type="terms"))
+		assigned_peaks$scaled_dtss = assigned_peaks$log_dtss-pred_log_dtss
+	}
+	
+	# Creating a column for enhancer distances
+	if ("enhancer" %in% reglocation) {
+		enhancers = chipenrich.data::enhancer.dnase_thurman.0
+		peak_mids = IRanges::mid(GenomicRanges::ranges(peakobj))
+		mids_gr = GenomicRanges::GRanges(
+			seqnames = GenomeInfoDb::seqnames(peakobj),
+			ranges = IRanges::IRanges(start = peak_mids, end = peak_mids),
+			name = GenomicRanges::mcols(peakobj)$name
+		)
+		enhancer_mids = IRanges::mid(GenomicRanges::ranges(enhancers))
+		enhancer_mids_gr = GenomicRanges::GRanges(
+			seqnames = GenomeInfoDb::seqnames(enhancers),
+			ranges = IRanges::IRanges(start = enhancer_mids, end = enhancer_mids),
+			name = GenomicRanges::mcols(enhancers)$name
+		)
+		dist_to_enh = GenomicRanges::distanceToNearest(mids_gr, enhancer_mids_gr)
+		assigned_peaks$dist_to_enh = dist_to_enh@elementMetadata$distance
+	}
+	
+	######################################################
+	# Enrichment
+	results = list()
+	for (gobj in geneset_list) {
+		message(sprintf("Test: Proximity test"))
+		message(sprintf("Genesets: %s",gobj@type))
+		message("Running tests..")
+		
+		if ("tss" %in% reglocation) {
+			message("Running proximity to TSS test...")
+			rtemp = test_proxReg(gobj, assigned_peaks, regloc = "tss", n_cores)
+		} else if ("enhancer" %in% reglocation) {
+			message("Running proximity to enhancers test...")
+			rtemp = test_proxReg(gobj, assigned_peaks, regloc = "enhancer", n_cores)
+		}
+		
+		# Annotate with geneset descriptions.
+		rtemp$"Description" = as.character(mget(rtemp$Geneset.ID, gobj@set.name, ifnotfound=NA))
+		rtemp$"Geneset.Type" = gobj@type
+		
+		results[[gobj@type]] = rtemp
+	}
+	enrich = Reduce(rbind,results)
+	
+	######################################################
+	# Post-process enrichment
+	# Order columns, add enriched/depleted column as needed, remove bad tests,
+	# sort by p-value, rename rownames to integers
+	enrich = post_process_enrichments(enrich)
+	
+	######################################################
+	# Write result objects to files
+	if (!is.null(out_name)) {
+		filename_analysis = file.path(out_path, sprintf("%s_results.tab", out_name))
+		write.table(enrich, file = filename_analysis, row.names = FALSE, quote = FALSE, sep = "\t")
+		message("Wrote results to: ", filename_analysis)
+		
+		filename_peaks = file.path(out_path, sprintf("%s_peaks.tab", out_name))
+		write.table(assigned_peaks, file = filename_peaks, row.names = FALSE, quote = FALSE, sep = "\t")
+		message("Wrote peak-to-gene assignments to: ", filename_peaks)
+		
+		filename_opts = file.path(out_path, sprintf("%s_opts.tab", out_name))
+		write.table(opts, file = filename_opts, row.names = FALSE, quote = FALSE, sep = "\t")
+		message("Wrote run options/arguments to: ", filename_opts)
+		
+		if (qc_plots) {
+			filename_qcplots = file.path(out_path, sprintf("%s_qcplots.png", out_name))
+			grDevices::png(filename_qcplots)
+			#print(..plot_proxReg_spline(peaks = assigned_peaks, num_peaks = num_peaks))
+			print(..plot_dist_to_tss(peakobj, tss))
+			grDevices::dev.off()
+			message("Wrote QC plots to: ",filename_qcplots)
+		}
+	}
+	
+	######################################################
+	# Return objects as list
+	return(list(
+		peaks = assigned_peaks,
+		results = enrich,
+		opts = opts
+	))
+	
+}
---
+++ R/test_proxReg.R
@@ -0,0 +1,92 @@
+test_proxReg = function(geneset, peaks, regloc, n_cores) {
+	# Restrict our genes/weights/peaks to only those genes in the genesets.
+	# Here, geneset is not all combined, but GOBP, GOCC, etc.
+	# i.e. A specific one.
+	peaks = subset(peaks, peaks$gene_id %in% geneset@all.genes)
+	
+	# Run tests. NOTE: If os == 'Windows', n_cores is reset to 1 for this to work
+	results_list = parallel::mclapply(as.list(ls(geneset@set.gene)), function(go_id) {
+		single_proxReg(go_id, geneset, peaks, regloc)}, mc.cores = n_cores)
+	
+	# Collapse results into one table
+	results = Reduce(rbind,results_list)
+	
+	# Correct for multiple testing
+	results$FDR = p.adjust(results$P.value, method="BH")
+	
+	# Create enriched/depleted status column
+	results$Status = ifelse(results$Effect > 0, 'enriched', 'depleted')
+	
+	results = results[order(results$P.value),]
+	
+	return(results)
+}
+
+single_proxReg = function(go_id, geneset, peaks, regloc) {
+
+	# Genes in the geneset
+	go_genes = geneset@set.gene[[go_id]]
+	
+	# Background genes and the background presence of a peak
+	b_genes = peaks$gene_id %in% go_genes
+	if (regloc == "tss"){
+		x = peaks$scaled_dtss[b_genes]
+		y = peaks$scaled_dtss[!b_genes]
+	}
+	if (regloc == "enhancer"){
+		x = peaks$dist_to_enh[b_genes]
+		y = peaks$dist_to_enh[!b_genes]
+	}
+	x <- x[is.finite(x)]
+	y <- y[is.finite(y)]
+	
+	# Information about the geneset
+	r_go_id = go_id
+	r_go_genes_num = length(go_genes)
+
+	# Information about peak genes
+	go_genes_peak = peaks$gene_id[b_genes]
+	r_go_genes_peak = length(table(go_genes_peak))
+	r_go_genes_peak_num = length(go_genes_peak)
+
+	r_effect = NA
+	r_pval = NA
+	
+	if (length(x)== 0L | length(y) == 0L) {
+		sprintf("Geneset: %s has zero peaks in one group. NAs given", go_id)
+	} else {
+		tryCatch(
+			{ #Code from wilcox.test.default
+			r <- rank(c(x, y))
+			n.x <- as.double(length(x))
+			n.y <- as.double(length(y))
+			STATISTIC <- c(W = sum(r[seq_along(x)]) - n.x * (n.x + 1)/2)
+			NTIES <- table(r)
+			z <- STATISTIC - n.x * n.y/2
+			SIGMA <- sqrt((n.x * n.y/12) * ((n.x + n.y + 1) - 
+							sum(NTIES^3 - NTIES)/((n.x + n.y) * (n.x + n.y - 
+								1))))
+			z = z/SIGMA
+			
+			# Results from the Wilcoxon test
+			r_effect = -z; #We want closer to be positive effect
+			r_pval = 2 * min(pnorm(z), 
+							 pnorm(z, lower.tail = FALSE))
+			},
+			error = {function(e) {warning(
+				sprintf("Error in geneset: %s. NAs given", go_id))
+			}}
+		)
+	}
+	
+	out = data.frame(
+		"P.value"=r_pval,
+		"Geneset ID"=r_go_id,
+		"N Geneset Genes"=r_go_genes_num,
+		"Geneset Peak Genes"=r_go_genes_peak,
+		"N Geneset Peak Genes"=r_go_genes_peak_num,
+		"Effect"=r_effect,
+		stringsAsFactors = FALSE)
+	
+	return(out)
+}
