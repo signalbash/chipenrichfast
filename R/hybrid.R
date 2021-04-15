3809e7b6aec525b9d74acc3571ffb2ee4837957b
kai <wangdaha@umich.edu>
2021-04-15 13:37:52 -0400

    update bioconductor

--- R/hybrid.R
+++ R/hybrid.R
@@ -117,8 +117,10 @@ hybridenrich <- function(	peaks,
     max_geneset_size = 2000,
     num_peak_threshold = 1,
     randomization = NULL,
-    n_cores = 1
+    n_cores = 1,
+    uuid=NULL
 ) {
+    message("UUID is:",uuid)
     genome = match.arg(genome)
     
     n_cores = reset_ncores_for_windows(n_cores)
